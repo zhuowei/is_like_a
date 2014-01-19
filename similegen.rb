c69d941d5a5dd223130e4708d76c8024dcef17fd
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-01-18 19:06:18 -0500

    Generate similes and publishes them every 10 minutes

---
+++ similegen.rb
@@ -0,0 +1,25 @@
+# load corpus
+
+module SimileGen
+	@adjpairs = []
+
+	data = File.read("sourcedata/sourcedata.txt")
+
+	data.lines.each do |l|
+		vals = l.sub("\n", "").split(",")
+		@adjpairs << vals
+	end
+
+	@template = "FIRST is like SECOND since they are both ADJECTIVE"
+
+	def self.make
+		# select a random adjective
+		adjarr = @adjpairs[Random.rand(@adjpairs.length)]
+		words = adjarr[1..-1]
+		first = words.delete_at(Random.rand(words.length))
+		second = words.delete_at(Random.rand(words.length))
+		out = @template.sub("ADJECTIVE", adjarr[0]).sub("FIRST", first).sub("SECOND", second)
+		return out
+	end
+end
+print SimileGen.make
