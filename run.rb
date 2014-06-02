69c38b3768e613185b0ae85bb88a437e89ef6847
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-06-01 23:48:52 -0700

    Added sl2c_ebooks

--- run.rb
+++ run.rb
@@ -5,6 +5,7 @@ require_relative 'sonnetbot'
 require_relative 'botstache_bot'
 require_relative 'aspects_bot'
 require_relative 'eyescaler_bot'
+require_relative 'standard_bots'
 
 EM.run do
  Ebooks::Bot.all.each do |bot|
---
+++ standard_bots.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'twitter_ebooks'
+require_relative 'aspects_bot'
+include Ebooks
+
+Ebooks::Bot.new("sl2c_ebooks") do |bot| # Ebooks account username
+  bot.consumer_key = ENV["CONSUMER_KEY"] # Your app consumer key
+  bot.consumer_secret = ENV["CONSUMER_SECRET"] # Your app consumer secret
+  bot.oauth_token = ENV["SL2C_TOKEN"] # oauth token for ebooks account
+  bot.oauth_token_secret = ENV["SL2C_SECRET"] # oauth secret for ebooks account
+
+  make_bot(bot, "sl2c") # This should be the name of the text model
+end
