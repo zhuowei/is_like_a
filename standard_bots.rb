fddc07bc3be7cc9c2ae2e0fbd29332878f64655f
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-06-02 22:17:21 -0700

    Added Munchlax_ebooks

--- standard_bots.rb
+++ standard_bots.rb
@@ -12,3 +12,12 @@ Ebooks::Bot.new("sl2c_ebooks") do |bot| # Ebooks account username
 
   make_bot(bot, "sl2c") # This should be the name of the text model
 end
+
+Ebooks::Bot.new("Munchlax_ebooks") do |bot| # Ebooks account username
+  bot.consumer_key = ENV["CONSUMER_KEY"] # Your app consumer key
+  bot.consumer_secret = ENV["CONSUMER_SECRET"] # Your app consumer secret
+  bot.oauth_token = ENV["MUNCHLAX_TOKEN"] # oauth token for ebooks account
+  bot.oauth_token_secret = ENV["MUNCHLAX_SECRET"] # oauth secret for ebooks account
+
+  make_bot(bot, "MunchlaxRegrets") # This should be the name of the text model
+end
