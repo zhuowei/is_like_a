3d36ffcc3156731451c21d5c83a03c063145a16a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-10-30 21:18:40 -0400

    Whoops, misspelled the method name

--- botstache_bot.rb
+++ botstache_bot.rb
@@ -14,7 +14,7 @@ Ebooks::Bot.new("botstache") do |bot|
 
   bot.on_mention do |tweet, meta|
     # Reply to a mention
-    image_bot_common(tweet, meta) do |img|
+    image_bot_handle(tweet, meta) do |img|
       Botstache.addstache(img)
     end
   end
